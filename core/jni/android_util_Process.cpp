873175617008862467dd3c4e17712404fc87d0bd
Tim Murray <timmurray@google.com>
2016-12-20 11:47:24 -0800

    Fix incorrect FIFO cgroup assignment.
    bug 33745094
    Test: builds
    
    Change-Id: I46eefc8ea6f17ca3795f4743692ddfea36d5db0f

--- core/jni/android_util_Process.cpp
+++ core/jni/android_util_Process.cpp
@@ -231,7 +231,7 @@ void android_os_Process_setProcessGroup(JNIEnv* env, jobject clazz, int pid, jin
         t_pri = getpriority(PRIO_PROCESS, t_pid);
 
         if (t_pri <= ANDROID_PRIORITY_AUDIO) {
-            int scheduler = sched_getscheduler(t_pid);
+            int scheduler = sched_getscheduler(t_pid) & ~SCHED_RESET_ON_FORK;
             if ((scheduler == SCHED_FIFO) || (scheduler == SCHED_RR)) {
                 // This task wants to stay in it's current audio group so it can keep it's budget
                 continue;
